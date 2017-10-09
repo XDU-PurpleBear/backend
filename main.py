36da5a7e9117b2c624507b134f2e713691041147
ganpengkang <ganpengkang@gmail.com>
2017-10-09 21:25:22 +0800

    api-v1

---
+++ main.py
@@ -0,0 +1,47 @@
+#
+#
+
+def login(UUID,password):
+
+    return
+
+def logout(UUID):
+
+    return
+
+def signup(username,password):
+
+    return UUID
+
+def UserInfoModified(firstname,lastname,birthday,registerdate,balance,sex,telephone,right):
+
+    return
+
+def query():
+
+#用户行为
+def UserBorrow():
+    #
+
+def UserReturn():
+    #
+
+def UserRenew():
+    #
+
+def UserOverdue():
+    #
+
+def AddLibrian():
+    #
+
+
+#图书馆管理员一系列操作
+def LibRemoveBook():
+    #
+
+def LibAddBook():
+    #
+
+def LibDeleteUser():
+    #
