ff73eca1d4d4b0c71d0b8d51161eb84ce6797bde
yanchejiu <yanchejiu@outlook.com>
2017-10-16 15:06:29 +0800

    Interface for John

--- John.py
+++ John.py
@@ -34,7 +34,7 @@ POST:
             }
 PUT:
     in:
-        token
+        right
     out:
         no_token_input:
             {
@@ -81,10 +81,78 @@ DELETE:
 
 
 import httplib
-body = {}
-headers = {}
-#conn.request('get', '/', body, headers)
+onlineUser = []
+putAuth = {
+    'no_token_input':{
+        'status':'error',
+        'msg':'need right',
+        'status-code':12
+        },
+    'token_right_wrong':{
+        'status':'success',
+        'nextExipre':0
+        },
+    'success':{
+                'status':'success',
+                'token':0,
+                'nextExipre':1666,
+                'status-code':0
+            }
+    }
+postAuth = {
+    'no_token_input':{
+        'status':'error',
+        'msg':'need token',
+        'status-code':10
+        },
+    'online_no_token':{
+        'status':'success',
+        'nextExipre':0
+        },
+    'system_error':{
+            'status':'error',
+            'msg':'error',
+            'status-code':11
+        },
+    'success':{
+                'status':'success',
+                'right':0,
+                'nextExipre':1666
+            }
+    }
+deleteAuth = {
+    'no_token_input':{
+        'status':'error',
+        'msg':'need token',
+        'status-code':10
+        },
+    'system_error':{
+            'status':'error',
+            'msg':'error',
+            'status-code':11
+        },
+    'success':{
+                'status':'success',
+                'status-code':30
+            }
+    }
+def deleteToken(username):
+    if username in onlineUser:
+        onlineUser.remove(username)
+        retInfo = deleteAuth['success']
+    else:
+        retInfo = deleteAuth['system_error']
+    return retInfo
 
-def connectionJohn(url):
-    conn = httplib.HTTPConnection()
-    return
+def getToken(username):
+    onlineUser.append(username)
+    retInfo = putAuth['success']
+    retInfo['token'] = 100000
+    return retInfo
+    
+def searchToken(username):
+    if username in onlineUser:
+        retInfo = postAuth['success']
+    else:
+        retInfo = postAuth['online_no_token']
+    return retInfo
