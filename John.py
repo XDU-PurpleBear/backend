4813631f417141d15978806319f905e8595015d6
yanchejiu <yanchejiu@outlook.com>
2017-10-27 10:42:35 +0800

    John part compeleted,need test

--- John.py
+++ John.py
@@ -34,7 +34,7 @@ POST:
             }
 PUT:
     in:
-        right
+        token
     out:
         no_token_input:
             {
@@ -81,78 +81,141 @@ DELETE:
 
 
 import httplib
-onlineUser = []
-putAuth = {
-    'no_token_input':{
-        'status':'error',
-        'msg':'need right',
-        'status-code':12
-        },
-    'token_right_wrong':{
-        'status':'success',
-        'nextExipre':0
-        },
-    'success':{
-                'status':'success',
-                'token':0,
-                'nextExipre':1666,
-                'status-code':0
-            }
-    }
-postAuth = {
-    'no_token_input':{
-        'status':'error',
-        'msg':'need token',
-        'status-code':10
-        },
-    'online_no_token':{
-        'status':'success',
-        'nextExipre':0
-        },
-    'system_error':{
-            'status':'error',
-            'msg':'error',
-            'status-code':11
-        },
-    'success':{
-                'status':'success',
-                'right':0,
-                'nextExipre':1666
-            }
-    }
-deleteAuth = {
-    'no_token_input':{
-        'status':'error',
-        'msg':'need token',
-        'status-code':10
-        },
-    'system_error':{
-            'status':'error',
-            'msg':'error',
-            'status-code':11
-        },
-    'success':{
-                'status':'success',
-                'status-code':30
-            }
-    }
-def deleteToken(username):
-    if username in onlineUser:
-        onlineUser.remove(username)
-        retInfo = deleteAuth['success']
-    else:
-        retInfo = deleteAuth['system_error']
-    return retInfo
+import urllib
+import json
 
-def getToken(username):
-    onlineUser.append(username)
-    retInfo = putAuth['success']
-    retInfo['token'] = 100000
+def addToJohn(data):
+    info = {}
+    retInfo = {}
+    info['data'] = data
+    info['method'] = 'PUT'
+    John = connectionJohn(info)
+    if John['status'] == 'error':
+        retInfo['status'] = 'failure'
+        if John['msg'] == 'need right':
+            retInfo['errorInfo'] = 'need right'
+        elif John['msg'] == 'need uuid':
+            retInfo['errorInfo'] = 'need uuid'
+        else:
+            retInfo['errorInfo'] = 'Unexpect operation'
+    elif John['status'] == 'success':
+        if John.has_key('token'):
+            retInfo['token'] = John['token']
+            retInfo['tokenDate'] = John['nextExpire']
+            retInfo['status'] = 'success'
+        else:
+            retInfo['errorInfo'] = 'wrong token'
+            retInfo['status'] = 'failure'
+    else:
+        retInfo['status'] = 'failure'
+        retInfo['errorInfo'] = 'Unexpect operation'
     return retInfo
     
-def searchToken(username):
-    if username in onlineUser:
-        retInfo = postAuth['success']
+def searchOnJohn(data):
+    info = {}
+    retInfo = {}
+    info['data'] = data
+    info['method'] = 'POST'
+    John = connectionJohn(info)
+    if John['status'] == 'error':
+        retInfo['status'] = 'failure'
+        if John['meg'] == 'need token':
+            retInfo['errorInfo'] = 'need token'
+        else:
+            retInfo['errorInfo'] = 'John System Error'
+    elif John['status'] == 'success':
+        if John.has_key('uuid'):
+            retInfo['uuid'] = John['uuid']
+            retInfo['right'] = John['right']
+            retInfo['tokenDate'] = John['nextExpire']
+            retInfo['status'] = 'success'
+        else:
+            retInfo['errorInfo'] = 'no user'
+            retInfo['status'] = 'failure'
     else:
-        retInfo = postAuth['online_no_token']
+        retInfo['status'] = 'failure'
+        retInfo['errorInfo'] = 'Unexpect operation'
     return retInfo
+    
+def deleteOnJohn(data):
+    info = {}
+    retInfo = {}
+    info['data'] = data
+    info['method'] = 'DELETE'
+    John = connectionJohn(info)
+    if John['status'] == 'error':
+        retInfo['status'] = 'failure'
+        if John['msg'] == 'need token':
+            retInfo['errorInfo'] = 'need token'
+        else:
+            retInfo['errorInfo'] = 'John System Error'
+    elif John['status'] == 'success':
+        retInfo['status'] = 'success'
+    else:
+        retInfo['status'] = 'failure'
+        retInfo['errorInfo'] = 'Unexpect operation'
+    return retInfo
+
+def connectionJohn(info):
+    data = info['data']
+    method = info['method']
+    conn = httplib.HTTPConnection('127.0.0.1:3000')
+    body = urllib.urlencode(data)
+    conn.request(method,'/aii/v1/auth' , body, {'user-agent':'uuid-test','Content-Type':'application/x-www-form-urlencoded'})
+    fromJohn = json.loads(conn.getresponse().read())
+    print fromJohn
+    conn.close()
+    return fromJohn
+    
+if __name__ == '__main__':
+    infos = {
+        'put':{
+            '0':{
+                'right':1
+            },
+            '1':{
+                'uuid':'12345'
+            },
+            '2':{
+                'right':1,'uuid':'12345'
+            },
+            '3':{
+                
+            }
+        },
+        'post':{
+            '0':{
+                'token':''
+            },
+            '1':{
+                'token':'12345'
+            },
+            '2':{
+                'token':'TG0JQ1o50fhEVvnlB4rTasmlyrY2gwNpIIUUtLHoBiNJcIVvDH2AycIYhhQV9oG'
+            },
+            '3':{
+                
+            },
+        },
+        'delete':{
+            '0':{
+                'token':''
+            },
+            '1':{
+                'token':'12345'
+            },
+            '2':{
+                'token':'TG0JQ1o50fhEVvnlB4rTasmlyrY2gwNpIIUUtLHoBiNJcIVvDH2AycIYhhQV9oG'
+            }
+        }
+    }
+    '''
+    fromJohn = addToJohn(data)
+    
+    print fromJohn
+    data['token'] = fromJohn['token']
+    fromJohn = searchOnJohn(data)
+    print fromJohn
+    fromJohn = deleteOnJohn(data)
+    print fromJohn
+    '''
